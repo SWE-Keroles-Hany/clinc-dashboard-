4db86ac30949fd025dc4d39886646eb484527b15
KerolesHany <keroleshany293@gmail.com>
2026-06-06 19:39:20 +0300

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ClincDashboard());
+}
+
+class ClincDashboard extends StatelessWidget {
+  const ClincDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Clinc Dashboard');
+  }
+}
